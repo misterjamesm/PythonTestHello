5a411554f016d6936dcbd5c4c2a5bbb5921b2d0c
J@mes <mister_jamesm@hotmail.com>
2021-07-24 10:11:09 +0200

    new release

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+@app.route("/<name>")
+def home(name):
+    return render_template("index.html", content=name)
+
+if __name__ == "__main__":
+    app.run()
